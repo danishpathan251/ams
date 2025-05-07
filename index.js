61864cade259b7e20e1e768f5a2cd8b1d8f1c980
eng.arkee <eng.arkee@gmail.com>
2025-05-07 12:20:36 +0530

    fix all new apies

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use('/api/business', businessRoute);
 
 // Example route
 app.get('/', (req, res) => {
-  res.send('Hello from Express! Updated At: 17:37 ');
+  res.send('Hello from Express! Updated At: 12:20 ');
 });
 
 
